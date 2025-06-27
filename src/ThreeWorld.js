3a88cf29853d2ac4ee5141bec9f6d0e11022a660
Wayne Stedman <waynestedman@users.noreply.github.com>
2025-06-27 10:47:28 -0700

    3world WIP

--- src/ThreeWorld.js
+++ src/ThreeWorld.js
@@ -1,5 +1,40 @@
 // three js world
+import * as THREE from 'three';
+import {OrbitControls} from 'three/addons/controls/OrbitControls.js';
 
-document.querySelector('#threeroot').innerHTML = `
-  <h2>3D Space</h2>
-`
+const worldroot = document.querySelector('#threeroot');
+const scene = new THREE.Scene();
+const camera = new THREE.PerspectiveCamera(75, window.innerWidth / window.innerHeight, 0.1, 1000);
+
+// renderer
+const renderer = new THREE.WebGLRenderer();
+renderer.setSize(worldroot.innerWidth, worldroot.innerHeight);
+renderer.setAnimationLoop(animate);
+worldroot.appendChild(renderer.domElement);
+
+// lighting
+const color = 0xFFFFFF;
+const intensity = 1;
+const light = new THREE.AmbientLight(color, intensity);
+scene.add(light);
+
+// geometry & materials
+const boxGeometry = new THREE.BoxGeometry(1, 1, 1);
+const material = new THREE.MeshBasicMaterial({ color: 0x00ff00 });
+const cube = new THREE.Mesh(boxGeometry, material);
+scene.add(cube);
+
+camera.position.z = 5;
+
+// controls
+// const controls = new OrbitControls(camera, canvas);
+// controls.target.set(0, 5, 0);
+// controls.update();
+
+// animation loop
+function animate() {
+  cube.rotation.x += 0.01;
+  cube.rotation.y += 0.01;
+
+  renderer.render(scene, camera);
+}
